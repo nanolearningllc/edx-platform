61be46ca58f6dded8132a6fa59158c66452483bf
Sarina Canelake <sarina@edx.org>
2014-05-19 15:24:36 -0400

    Add Oriya language

--- lms/envs/common.py
+++ lms/envs/common.py
@@ -605,6 +605,7 @@ LANGUAGES = (
     ('nb', u'Norsk bokmål'),  # Norwegian Bokmål
     ('ne', u'नेपाली'),  # Nepali
     ('nl-nl', u'Nederlands (Nederland)'),  # Dutch (Netherlands)
+    ('or', u'ଓଡ଼ିଆ'),  # Oriya
     ('pl', u'Polski'),  # Polish
     ('pt-br', u'Português (Brasil)'),  # Portuguese (Brazil)
     ('pt-pt', u'Português (Portugal)'),  # Portuguese (Portugal)
