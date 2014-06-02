c6e87428a51364b5c657f8fa48c8e9bbfa853da2
Nimisha Asthagiri <nasthagiri@edx.org>
2014-06-02 16:41:44 -0400

    LMS fixes.

--- lms/djangoapps/courseware/tests/modulestore_config.py
+++ lms/djangoapps/courseware/tests/modulestore_config.py
@@ -3,7 +3,6 @@ Define test configuration for modulestores.
 """
 
 from xmodule.modulestore.tests.django_utils import xml_store_config, \
-    draft_mongo_store_config,\
     mixed_store_config
 
 from django.conf import settings
@@ -30,4 +29,4 @@ TEST_DATA_MIXED_MODULESTORE = mixed_store_config(TEST_DATA_DIR, MAPPINGS)
 # All store requests now go through mixed
 # NAATODO - get rid of these constants?
 TEST_DATA_DRAFT_MONGO_MODULESTORE = TEST_DATA_MIXED_MODULESTORE
-TEST_DATA_MONGO_MODULESTORE = TEST_DATA_DRAFT_MONGO_MODULESTORE
+TEST_DATA_MONGO_MODULESTORE = TEST_DATA_MIXED_MODULESTORE
