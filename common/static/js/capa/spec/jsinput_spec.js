dcd4809b0ba70cb3315c724101c4e922ddc3dd5b
stv <stv@stanford.edu>
2014-06-04 18:41:35 -0700

    Select DOM elements with wildcard syntax
    Lookups were being done with non-existent literal selectors, so it
    was returning empty lists. As assertions were to be made while iterating
    over the list of elements, nothing was actually being verified.
    
    Common code has been centralized in the setup function. By declaring CSS
    selectors once, we minimize the odds of inadvertently using the wrong
    selector, as happened here.

--- common/static/js/capa/spec/jsinput_spec.js
+++ common/static/js/capa/spec/jsinput_spec.js
@@ -1,19 +1,20 @@
 describe("JSInput", function() {
     beforeEach(function () {
         loadFixtures('js/capa/fixtures/jsinput.html');
+        this.sections = $(document).find('section[id^="inputtype_"]');
+        this.inputFields = $(document).find('input[id^="input_"]');
+        JSInput.walkDOM();
     });
 
     it('sets all data-processed attributes to true on first load', function() {
-        var sections = $(document).find('section[id="inputtype_"]');
-        JSInput.walkDOM();
-        sections.each(function(index, section) {
-            expect(section.attr('data-processed')).toEqual('true');
+        this.sections.each(function(index, item) {
+            expect(item.attr('data-processed')).toEqual('true');
         });
     });
 
     it('sets the data-processed attribute to true on subsequent load', function() {
-        var section1 = $(document).find('section[id="inputtype_1"]'),
-            section2 = $(document).find('section[id="inputtype_2"]');
+        var section1 = $(this.sections[0]);
+        var section2 = $(this.sections[1]);
         section1.attr('data-processed', false);
         JSInput.walkDOM();
         expect(section1.attr('data-processed')).toEqual('true');
@@ -21,18 +22,14 @@ describe("JSInput", function() {
     });
 
     it('sets the waitfor attribute to its update function', function() {
-        var inputFields = $(document).find('input[id="input_"]');
-        JSInput.walkDOM();
-        inputFields.each(function(index, inputField) {
-            expect(inputField.data('waitfor')).toBeDefined();
+        this.inputFields.each(function(index, item) {
+            expect(item.data('waitfor')).toBeDefined();
         });
     });
 
     it('tests the correct number of sections', function () {
-        var sections = $(document).find('section[id="inputtype_"]');
-        var inputFields = $(document).find('input[id="input_"]');
-        expect(sections.length).toEqual(inputFields.length);
-        expect(sections.length).toBeGreaterThan(0);
+        expect(this.sections.length).toEqual(this.inputFields.length);
+        expect(this.sections.length).toBeGreaterThan(0);
     });
 });
 
